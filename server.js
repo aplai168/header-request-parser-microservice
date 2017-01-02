ab7edbe09b18fdf2a39d859cd2289d4dc667ae39
Alice Lai <aplai168@gmail.com>
2017-01-02 23:10:36 +0000

    finished

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-var express = require('express')
-var app = express()
+'use strict';
 
-app.get('/', function (req, res) {
-  res.send('This is the response that sends whatever you want!')
-})
-
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+module.exports = function(app) {
+  app.route('/whoami')
+    .get(function(req, res) {
+      var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+     var info = {
+         'ipaddress': ip,
+         'language': req.headers["accept-language"].split(',')[0],
+         'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
+     };
+     res.send(info);
+    });
+}; 
